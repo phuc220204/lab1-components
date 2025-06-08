36a3a3269959e5423ba833d98f0ca4afd2b9fe29
Developer <dev@example.com>
2025-06-08 18:47:03 +0700

    chore: update title, favicon and add global page padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ export default function AppNavbar() {
   return (
     <Navbar expand="lg" className="navbar-custom">
       <Container fluid className="px-0">
-        <Navbar.Brand href="#">🌸 Orchids</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
+        <Navbar.Brand href="#" className="d-flex align-items-center">
+          🌸 <span className="ms-2">Orchids</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
