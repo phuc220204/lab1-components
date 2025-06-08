c630b1ece37dacefb098ee7a90144ca351a8d2d7
Developer <dev@example.com>
2025-06-08 17:44:57 +0700

    feat: add Navbar with Home/About/Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AppNavbar from "./components/Navbar";
 import Orchids from "./components/Orchids";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <AppNavbar />
       <h1>Orchids Gallery</h1>
       <Orchids />
     </div>
