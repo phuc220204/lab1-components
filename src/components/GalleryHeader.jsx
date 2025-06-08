7442057136ab172dc54ffa9719790005ec8d804b
Developer <dev@example.com>
2025-06-08 21:01:33 +0700

    style: center "Orchids Gallery" header

--- src/components/GalleryHeader.jsx
+++ src/components/GalleryHeader.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 export default function GalleryHeader() {
   return (
-    <div className="gallery-header">
-      <h1>Orchids Gallery</h1>
-    </div>
+    <Container fluid>
+      <div className="gallery-header">
+        <h1>Orchids Gallery</h1>
+      </div>
+    </Container>
   );
 }
