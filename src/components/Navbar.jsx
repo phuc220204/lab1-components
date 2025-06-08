8fbb19b86769a17f5da85a9479f6831f98a7bb6d
Developer <dev@example.com>
2025-06-08 18:10:33 +0700

    chore: practice Navbar component full-width and simple styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function AppNavbar() {
   return (
-    <Navbar expand="lg" sticky="top" className="navbar-custom" variant="light">
+    <Navbar expand="lg" sticky="top" className="navbar-custom">
       <Container fluid className="px-0">
-        <Navbar.Brand href="#">Orchids</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Brand href="#">🌸 Orchids</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
           <Nav className="ms-auto">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
