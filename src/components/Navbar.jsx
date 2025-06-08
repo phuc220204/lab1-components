260ea552d809c66cf25c7082f4c6860020bde42c
Developer <dev@example.com>
2025-06-08 17:46:40 +0700

    enhance: improve navbar styling and mobile responsiveness

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+export default function AppNavbar() {
+  return (
+    <Navbar expand="lg" className="navbar-custom" variant="light">
+      <Container>
+        <Navbar.Brand href="#">Orchids</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
